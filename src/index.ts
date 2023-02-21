7ee53f125b5900fd4d916df325b277e1c6469e25
Nate Taylor <nate.taylor@aggiemail.usu.edu>
2023-02-20 17:00:13 -0700

    added migration for all models and test for server in index

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,30 @@ const client = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
-  const user = await client.user.create({
-    data: {
-      firstName: "Joseph",
-      lastName: "Ditton",
-      email: "joseph.ditton@usu.edu",
-      passwordHash: "q23oejklnvzlskjfdnf"
-    }
-  });
-  res.json({ user });
-});
-
-app.get("/users", async (req, res) => {
-  const users = await client.user.findMany();
-  res.json({ users });
+app.get("/hello", async (req, res) => {
+  console.log("it worked");
 })
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello, world!</h1>`);
-});
+// app.post('/users', async (req, res) => {
+//   const user = await client.user.create({
+//     data: {
+//       firstName: "Joseph",
+//       lastName: "Ditton",
+//       email: "joseph.ditton@usu.edu",
+//       passwordHash: "q23oejklnvzlskjfdnf"
+//     }
+//   });
+//   res.json({ user });
+// });
+
+// app.get("/users", async (req, res) => {
+//   const users = await client.user.findMany();
+//   res.json({ users });
+// })
+
+// app.get("/", (req, res) => {
+//   res.send(`<h1>Hello, world!</h1>`);
+// });
 
 app.listen(3000, () => {
   console.log("I got started!");
