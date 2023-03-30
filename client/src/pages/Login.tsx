59c6f2fa017bb9b71e6f2dec82293b07be841ccb
ClarkFarnswoth <farnsworthclark@gmail.com>
2023-03-29 20:01:56 -0600

    Worked on login in page.

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,8 +3,29 @@
 - I should be able to navigate to the signup page
 - Upon signing in, I should be redirected to the dashboard page
 */
+import { useContext, useState } from 'react';
+import { useApi } from '../hooks/useApi';
 import './Login.css'
 export const Login = () => {
+  
+  const [email, getEmail] = useState("");
+  const [password, getPassword] = useState("");
+  const api = useApi();
+  
+
+  async function login() {
+    const body = {
+      email,
+      password
+    }
+
+    const resultBody = await api.post(`${import.meta.env.DATABASE_URL}/users`, body)
+
+    if (resultBody.token) {
+      
+    }
+  }
+
   
     return  <div className="login">
       <div className="company">
